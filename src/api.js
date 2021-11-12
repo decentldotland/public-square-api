e7ea322af298d5b9564714ae08f6a0024d16e944
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-11-12 12:49:50 +0200

    feat: decoding post data

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
 import { arweave } from "./utils/arweave/arweave.js";
 import { _validateAddress } from "./utils/arweave/address.js";
+import { _decodePostsData } from "./utils/arweave/arweave.js";
 import {
   postQuerySchema,
   postsPerAddressQuerySchema,
+  arweaveSavesQuerySchema,
   gqlTemplate,
 } from "./utils/arweave/gql.js";
 
@@ -23,9 +25,11 @@ export async function getPostsOf(address) {
     _validateAddress(address);
     const query = postsPerAddressQuerySchema(address);
     const transactions = await gqlTemplate(query);
-    return transactions;
+    const feed = await _decodePostsData(transactions, address);
+
+    return feed;
   } catch (error) {
-    console.log(error.name);
+    console.log(`${error.name}: ${error.message}`);
     process.exit(1);
   }
 }
@@ -35,23 +39,7 @@ export async function getFeed() {
 
   try {
     const postsTxs = await getPostsTransactions();
-
-    for (let tx of postsTxs) {
-      const txObject = await arweave.transactions.get(tx);
-      const txData = await arweave.transactions.getData(tx, {
-        decode: true,
-        string: true,
-      });
-
-      const owner = txObject["owner"];
-      const poster = await arweave.wallets.ownerToAddress(owner);
-
-      feed.push({
-        id: tx,
-        poster: poster,
-        data: txData,
-      });
-    }
+    const feed = await _decodePostsData(postsTxs, false);
 
     return feed;
   } catch (error) {
@@ -60,5 +48,44 @@ export async function getFeed() {
   }
 }
 
+export async function getArweaveSaves() {
+  try {
+    const savesArray = [];
+    const saves = await gqlTemplate(arweaveSavesQuerySchema);
 
+    for (let tx of saves) {
+      const txStatus = (await arweave.transactions.getStatus(tx)).status;
 
+      if (txStatus !== 200) {
+        continue;
+      }
+      const tagsMap = new Map();
+      const txObject = await arweave.transactions.get(tx);
+      const tags = txObject.get("tags");
+
+      for (let tag of tags) {
+        const key = tag.get("name", { decode: true, string: true });
+        const value = tag.get("value", { decode: true, string: true });
+        tagsMap.set(key, value);
+      }
+
+      savesArray.push({
+        txid: tx,
+        url: tagsMap.has("page:url")
+          ? tagsMap.get("page:url")
+          : "https://arweave.net",
+        title: tagsMap.has("page:title")
+          ? tagsMap.get("page:title")
+          : undefined,
+        timestamp: tagsMap.has("page:timestamp")
+          ? tagsMap.get("page:timestamp")
+          : 1984,
+      });
+    }
+
+    return savesArray;
+  } catch (error) {
+    console.log(`${error.name}: ${error.message}`);
+    process.exit(1);
+  }
+}

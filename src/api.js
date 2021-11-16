5446f678e8c7819ae20af87101a2a10b1d831fb9
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-11-16 17:14:18 +0200

    feat: better graphQL queries

--- src/api.js
+++ src/api.js
@@ -4,7 +4,6 @@ import { _decodePostsData } from "./utils/arweave/arweave.js";
 import {
   postQuerySchema,
   postsPerAddressQuerySchema,
-  arweaveSavesQuerySchema,
   gqlTemplate,
 } from "./utils/arweave/gql.js";
 
@@ -25,7 +24,7 @@ export async function getPostsOf(address) {
     _validateAddress(address);
     const query = postsPerAddressQuerySchema(address);
     const transactions = await gqlTemplate(query);
-    const feed = await _decodePostsData(transactions, address);
+    const feed = await _decodePostsData(transactions);
 
     return feed;
   } catch (error) {
@@ -39,7 +38,7 @@ export async function getFeed() {
 
   try {
     const postsTxs = await getPostsTransactions();
-    const feed = await _decodePostsData(postsTxs, false);
+    const feed = await _decodePostsData(postsTxs);
 
     return feed;
   } catch (error) {
@@ -48,44 +47,4 @@ export async function getFeed() {
   }
 }
 
-export async function getArweaveSaves() {
-  try {
-    const savesArray = [];
-    const saves = await gqlTemplate(arweaveSavesQuerySchema);
 
-    for (let tx of saves) {
-      const txStatus = (await arweave.transactions.getStatus(tx)).status;
-
-      if (txStatus !== 200) {
-        continue;
-      }
-      const tagsMap = new Map();
-      const txObject = await arweave.transactions.get(tx);
-      const tags = txObject.get("tags");
-
-      for (let tag of tags) {
-        const key = tag.get("name", { decode: true, string: true });
-        const value = tag.get("value", { decode: true, string: true });
-        tagsMap.set(key, value);
-      }
-
-      savesArray.push({
-        txid: tx,
-        url: tagsMap.has("page:url")
-          ? tagsMap.get("page:url")
-          : "https://arweave.net",
-        title: tagsMap.has("page:title")
-          ? tagsMap.get("page:title")
-          : undefined,
-        timestamp: tagsMap.has("page:timestamp")
-          ? tagsMap.get("page:timestamp")
-          : 1984,
-      });
-    }
-
-    return savesArray;
-  } catch (error) {
-    console.log(`${error.name}: ${error.message}`);
-    process.exit(1);
-  }
-}

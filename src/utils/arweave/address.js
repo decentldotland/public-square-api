9fa8d75495bb739fe150330fd05411e078f2cbde
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-11-18 23:12:55 +0200

    fix: backfall feed

--- src/utils/arweave/address.js
+++ src/utils/arweave/address.js
@@ -3,9 +3,13 @@ import { InvalidArweaveAddress } from "../errors/invalidAddress.js";
 
 
 export function _validateAddress(address) {
-	const validity = /[a-z0-9_-]{43}/i.test(address)
+  const validity = /[a-z0-9_-]{43}/i.test(address);
 
-	if (! validity) {
-		throw new InvalidArweaveAddress(`address: ${address} is not valid`)
-	}
+  if (!validity) {
+    // throw new InvalidArweaveAddress(`address: ${address} is not valid`)
+    // return a default feed of an automated publisher
+    return "vZY2XY1RD9HIfWi8ift-1_DnHLDadZMWrufSh-_rKF0";
+  }
+
+  return address;
 }

31a0369b31bf5ccaed80d132375ba869209b816a
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-11-23 21:53:49 +0200

    feat: post replies & fetch replies 💬

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import { arweave } from "./utils/arweave/arweave.js";
 import { _validateAddress, isParsable } from "./utils/arweave/address.js";
 import { _decodePostsData } from "./utils/arweave/arweave.js";
-import { createPost } from "./utils/arweave/arconnect.js";
+import { createPost, createReply } from "./utils/arweave/arconnect.js";
 import {
   postQuerySchema,
   postsPerAddressQuerySchema,
+  repliesOfPostQuerySchema,
   gqlTemplate,
 } from "./utils/arweave/gql.js";
 
@@ -20,9 +21,21 @@ export async function getPostsTransactions() {
   }
 }
 
+async function getPostRepliesTxs(post_id) {
+  try {
+    _validateAddress(post_id, false);
+    const query = repliesOfPostQuerySchema(post_id);
+    const transactions = await gqlTemplate(query);
+
+    return transactions;
+  } catch (error) {
+    console.log(`${error.name} : ${error.value}`);
+  }
+}
+
 export async function getPostsOf(address) {
   try {
-    const poster = _validateAddress(address);
+    const poster = _validateAddress(address, true);
     const query = postsPerAddressQuerySchema(poster);
     const transactions = await gqlTemplate(query);
     const feed = await _decodePostsData(transactions);
@@ -50,8 +63,8 @@ export async function getFeed() {
 export async function post(text, media) {
   try {
     const postTxObj = await createPost({
-      text: text,
-      media: media,
+      text,
+      media,
     });
     return postTxObj;
   } catch (error) {
@@ -59,14 +72,27 @@ export async function post(text, media) {
   }
 }
 
+export async function reply(text, media, post_id) {
+  try {
+    const replyTxObj = await createReply({
+      text,
+      media,
+      post_id,
+    });
+    return replyTxObj;
+  } catch (error) {
+    console.log(`${error.name}: ${error.message}`);
+  }
+
+
 export async function getLazyFeed() {
   try {
     const feed = [];
     const posts = await getPostsTransactions();
 
     for (let post of posts) {
-      const base64urlTag = post.tags[8]["value"];
-      const encodedContent = base64url.decode(base64urlTag);
+      const base64urlTag = post.tags.find(tag => tag.name === "lazyAccess")
+      const encodedContent = base64url.decode(base64urlTag["value"]);
 
       if (isParsable(encodedContent)) {
         feed.push({
@@ -83,3 +109,29 @@ export async function getLazyFeed() {
     console.log(`${error.name}: ${error.message}`);
   }
 }
+
+export async function getLazyReplies(post_id) {
+  try {
+    const feed = [];
+    const replies = await getPostRepliesTxs(post_id);
+
+    for (let reply of replies) {
+      const base64urlTag = reply.tags.find((tag) => tag.name === "lazyAccess");
+      const encodedContent = base64url.decode(base64urlTag["value"]);
+
+      if (isParsable(encodedContent)) {
+        feed.push({
+          rid: reply.id,
+          poster: reply.owner,
+          timestamp: reply.timestamp,
+          data: JSON.parse(encodedContent),
+        });
+      }
+    }
+
+    return feed;
+  } catch (error) {
+    console.log(`${error.name}: ${error.message}`);
+  }
+}
+
